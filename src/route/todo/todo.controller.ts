3579f679fe805db885fdb5773871d14469f549ff
renmurakami <autoeyy@gmail.com>
2023-04-20 02:22:34 +0900

    feat(todo): create todo controller and service

---
+++ src/route/todo/todo.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+	//get todo list
+});
+
+router.post("/", (req: Request, res: Response) => {
+	//create todo list
+});
