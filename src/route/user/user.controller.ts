a4915e4d451d61d0c12da04cb38ae86cd82719da
renmurakami <autoeyy@gmail.com>
2023-04-20 15:56:50 +0900

    userwithtodo

--- src/route/user/user.controller.ts
+++ src/route/user/user.controller.ts
@@ -1,10 +1,14 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userService } from "./user.service";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-	userService.getUser(res, req.payload.id);
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+	userService.getUser(res, req.payload.id, next);
+});
+
+router.get("/withTodo", (req: Request, res: Response, next: NextFunction) => {
+	userService.getUserWithTodo(res, req.payload.id, next);
 });
 
 export default router;
--- src/route/user/user.service.ts
+++ src/route/user/user.service.ts
@@ -1,22 +1,44 @@
 import { PrismaClient, User } from "@prisma/client";
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { exclude } from "../../util/exclude";
 
 const prisma = new PrismaClient();
 
-const getUser = async (res: Response, userId: number) => {
-	const user = await prisma.user.findUnique({
-		where: {
-			id: userId,
-		},
-	});
+const getUser = async (res: Response, userId: number, next: NextFunction) => {
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+		});
 
-	if (user) {
-		const formatUser = exclude<User, keyof User>(user, ["password"]);
-		res.json({ formatUser });
+		if (user) {
+			const formatUser = exclude<User, keyof User>(user, ["password"]);
+			res.json({ formatUser });
+		}
+	} catch (e) {
+		next(e);
+	}
+};
+
+const getUserWithTodo = async (res: Response, userId: number, next: NextFunction) => {
+	try {
+		const userWithTodo = await prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+			include: {
+				todos: true,
+			},
+		});
+
+		res.json({ userWithTodo });
+	} catch (e) {
+		next(e);
 	}
 };
 
 export const userService = {
 	getUser,
+	getUserWithTodo,
 };
